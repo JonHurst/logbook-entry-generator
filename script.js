aaaf08252a44552ca390949fd4feb52cc7f0502b
Jon Hurst <jon@hursts.org.uk>
2019-02-05 13:43:33 +0000

    Use readonly textareas

--- script.js
+++ script.js
@@ -92,12 +92,10 @@ function spacepadNumber(number, length) {
 
 function sectorsFragment(sectors) {
     if(sectors.length == 0) return null;
-    var list = $("<ul/>");
+    var text = "";
     for(var c = 0; c < sectors.length; c++) {
         var s = sectors[c];
-        var li = $("<li/>");
-        li.append($("<pre/>").text(
-            s.Start + ", " +
+        text += s.Start + ", " +
                 spacepadNumber(s.Duration, 3) + ", " +
                 spacepadNumber(s.NightDuration, 3) + ", \"" +
                 s.From + "\", \""
@@ -105,37 +103,32 @@ function sectorsFragment(sectors) {
                 s.Registration + "\", \"" +
                 s.Type + "\", \"" +
                 s.Captain + "\", \"" +
-                s.Role + "\""));
-        list.append(li);
+                s.Role + "\"&#10;";
     }
-    return list;
+    return text;
 }
 
 
 function dutiesFragment(duties) {
     if(duties.length == 0) return null;
-    var ul = $("<ul/>");
+    var text = "";
     for(var c = 0; c < duties.length; c++) {
         var d = duties[c];
-        var li = $("<li/>");
-        li.append($("<pre/>").text(
-            d.Start + ", " +
-                spacepadNumber(d.Duration, 3)));
-        ul.append(li);
+        text += d.Start + ", " +
+            spacepadNumber(d.Duration, 3) + "&#10;";
     }
-    return ul;
+    return text;
 }
 
 
 function crewFragment(sectors) {
     var crew_found = false;
-    var ul = $("<ul/>");
+    var text = "";
     for(var c = 0; c < sectors.length; c++) {
         var s = sectors[c];
         if(s.Crew.length == 0) continue;
         crew_found = true;
-        var li = $("<li/>");
-        var text = s.Start + ", ";
+        text += s.Start + ", ";
         for(var cc = 0; cc < s.Crew.length; cc++) {
             //need to remove any " as we're outputting csv
             text += "\"" +
@@ -144,11 +137,14 @@ function crewFragment(sectors) {
                 s.Crew[cc][1].replace(/"/g, "") +
                 "\", ";
         }
-        li.append($("<pre/>").text(
-            text.slice(0, -2)));
-        ul.append(li);
+        text = text.slice(0, -2) + "&#10;";
     }
-    return crew_found ? ul : null;
+    return text;
+}
+
+
+function textarea(str) {
+    return "<textarea readonly='readonly' rows='10'>" + str + "</textarea>";
 }
 
 
@@ -169,17 +165,17 @@ function displayData(data) {
         var sector_frag = sectorsFragment(data.sectors);
         if(sector_frag) $("#sectors")
             .append($("<h1>Sectors</h1>"))
-            .append(sector_frag)
+            .append($(textarea(sector_frag)))
             .removeClass("hidden");
         var duties_frag = dutiesFragment(data.duties);
         if (duties_frag) $("#duties")
             .append($("<h1>Duties</h1>"))
-            .append(duties_frag)
+            .append($(textarea(duties_frag)))
             .removeClass("hidden");
         var crew_frag = crewFragment(data.sectors);
         if (crew_frag) $("#crew")
             .append($("<h1>Crew</h1>"))
-            .append($(crew_frag))
+            .append($(textarea(crew_frag)))
             .removeClass("hidden");
     }
 }
